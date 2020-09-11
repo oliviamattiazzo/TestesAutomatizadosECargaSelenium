7e76a1ff6d07de4e9e27462c618bd06e5af6ad3b
Olivia Mattiazzo <oli.pmatt@gmail.com>
2020-09-11 16:28:20 +0100

    Started new project

---
+++ TesteAutomatizadoSelenium/BuscaGoogleTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+
+namespace TesteAutomatizadoSelenium
+{
+    [TestClass]
+    public class BuscaGoogleTest
+    {
+        IWebDriver _driver;
+        WebDriverWait _espera;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _driver = new InternetExplorerDriver(ConfigurationManager.AppSettings["URLDriverIE"]);
+            _espera = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+
+            _driver.Manage().Window.Maximize();
+            _driver.Navigate().GoToUrl(ConfigurationManager.AppSettings["URLBuscaGoogleTest"]);
+        }
+
+        [TestMethod]
+        public void BuscarSeleniuWebDriver_PrimeiroResultadoEnderecoSiteSelenium()
+        {
+            IWebElement caixaPesquisaGoogle = _driver.FindElement(By.XPath("//input[@title='Pesquisar']"));
+            caixaPesquisaGoogle.SendKeys("Selenium WebDriver");
+
+            IWebElement btnPesquisaGoogle = _driver.FindElement(By.XPath("//input[@name='btnK']"));
+            btnPesquisaGoogle.SendKeys(Keys.Enter);
+
+            IWebElement primeiroResultado = _espera.Until(ExpectedConditions.ElementExists(By.ClassName("rc")));
+
+            Assert.IsNotNull(primeiroResultado.FindElement(By.ClassName("r")).FindElement(By.XPath("//a[@href='https://www.selenium.dev/projects/']")));
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            _driver.Close();
+            _driver.Quit();
+        }
+    }
+}
---
+++ TesteAutomatizadoSelenium/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TesteAutomatizadoSelenium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TesteAutomatizadoSelenium")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0fc980ae-f41d-4c29-b346-99a80d6135ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
